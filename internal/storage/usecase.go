d79c0d96fe0453f78b06e8ee63d8b0574095bc05
diyliv <onlogn081@gmail.com>
2023-01-17 22:47:00 +0300

    usecase interface

--- internal/storage/usecase.go
+++ internal/storage/usecase.go
@@ -1,4 +1,14 @@
 package storage
 
+import (
+	"context"
+
+	"github.com/diyliv/storage/internal/models"
+)
+
 type Usecase interface {
+	Register(ctx context.Context, user models.User) error
+	GetUserInfo(ctx context.Context, email string) (models.User, error)
+	CreateSession(ctx context.Context, id int, sessionToken string) error
+	SavePublicKey(ctx context.Context, key string) error
 }
