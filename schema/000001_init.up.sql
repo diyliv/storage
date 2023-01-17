2b5684b6a253c0f8a7564aa28f9b17194f185ffc
diyliv <onlogn081@gmail.com>
2023-01-17 22:49:47 +0300

    first example of schema

---
+++ schema/000001_init.up.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS users (
+    user_id SERIAL PRIMARY KEY, 
+    user_name VARCHAR(16) NOT NULL,
+    user_email VARCHAR(32) NOT NULL UNIQUE, 
+    user_hashed_password VARCHAR NOT NULL, 
+    user_updated_password TIMESTAMP DEFAULT NOW() NOT NULL, 
+    user_created_at TIMESTAMP DEFAULT NOW() NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users_keys(
+    id INT NOT NULL, 
+    user_public_key VARCHAR NOT NULL,
+    user_passphrase VARCHAR(16) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS sessions(
+    id INT NOT NULL, 
+    session_token VARCHAR NOT NULL
+);
