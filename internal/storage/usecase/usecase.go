f614a1001436a6be05489a96f00fed086c20fbcf
diyliv <onlogn081@gmail.com>
2023-01-17 22:51:28 +0300

    usecase.go

--- internal/storage/usecase/usecase.go
+++ internal/storage/usecase/usecase.go
@@ -1,6 +1,11 @@
 package usecase
 
-import "github.com/diyliv/storage/internal/storage"
+import (
+	"context"
+
+	"github.com/diyliv/storage/internal/models"
+	"github.com/diyliv/storage/internal/storage"
+)
 
 type storageUC struct {
 	postgresRepo storage.PostgresRepository
@@ -8,4 +13,20 @@ type storageUC struct {
 
 func NewStorageUC(postgresRepo storage.PostgresRepository) *storageUC {
 	return &storageUC{postgresRepo: postgresRepo}
+}
+
+func (s *storageUC) Register(ctx context.Context, user models.User) error {
+	return s.postgresRepo.Register(ctx, user)
+}
+
+func (s *storageUC) GetUserInfo(ctx context.Context, email string) (models.User, error) {
+	return s.postgresRepo.GetUserInfo(ctx, email)
+}
+
+func (s *storageUC) CreateSession(ctx context.Context, id int, sessionToken string) error {
+	return s.postgresRepo.CreateSession(ctx, id, sessionToken)
+}
+
+func (s *storageUC) SavePublicKey(ctx context.Context, key string) error {
+	return s.postgresRepo.SavePublicKey(ctx, key)
 }
