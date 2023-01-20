6727e483bec72ff953464555c6946a26d0e1055b
diyliv <onlogn081@gmail.com>
2023-01-20 18:35:54 +0300

    methods for redis

--- internal/storage/usecase.go
+++ internal/storage/usecase.go
@@ -9,6 +9,7 @@ import (
 type Usecase interface {
 	Register(ctx context.Context, user models.User) error
 	GetUserInfo(ctx context.Context, email string) (models.User, error)
-	CreateSession(ctx context.Context, id int, sessionToken string) error
+	CreateSession(ctx context.Context, userId, userName, userEmail, sessionToken string) error
+	CheckToken(ctx context.Context, sessionToken string) error
 	SavePublicKey(ctx context.Context, key string) error
 }
