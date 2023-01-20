73172fd7d6b04f3442983fe38ac1cecc10aabeb1
diyliv <onlogn081@gmail.com>
2023-01-20 18:35:34 +0300

    RedisRepository interface

--- internal/storage/repository.go
+++ internal/storage/repository.go
@@ -9,6 +9,10 @@ import (
 type PostgresRepository interface {
 	Register(ctx context.Context, user models.User) error
 	GetUserInfo(ctx context.Context, email string) (models.User, error)
-	CreateSession(ctx context.Context, id int, sessionToken string) error
 	SavePublicKey(ctx context.Context, key string) error
+}
+
+type RedisRepository interface {
+	CreateSession(ctx context.Context, userId, userName, userEmail, sessionToken string) error
+	CheckToken(ctx context.Context, sessionToken string) error
 }
