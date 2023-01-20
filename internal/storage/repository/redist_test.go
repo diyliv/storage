0e87d66daf45707edf1b327dc50b5319870872c9
diyliv <onlogn081@gmail.com>
2023-01-20 23:23:08 +0300

    added mock

--- internal/storage/repository/redist_test.go
+++ internal/storage/repository/redist_test.go
@@ -3,21 +3,45 @@ package repository
 import (
 	"context"
 	"testing"
+	"time"
+
+	"github.com/alicebob/miniredis/v2"
+	"github.com/go-redis/redis"
 
 	"github.com/diyliv/storage/config"
 	"github.com/diyliv/storage/pkg/logger"
-	"github.com/diyliv/storage/pkg/storage/redis"
 )
 
 var (
-	cfg        = config.ReadConfig("../../../config")
-	log        = logger.InitLogger()
-	redisConn  = redis.ConnRedis(cfg)
-	redisLogic = NewRedisRepo(log, redisConn, cfg)
-	ctx        = context.Background()
+	cfg = config.ReadConfig("../../../config")
+	log = logger.InitLogger()
+	ctx = context.Background()
 )
 
+func ConnRedis(t *testing.T) *redis.Client {
+	mock, err := miniredis.Run()
+	if err != nil {
+		t.Errorf("Error while starting miniredis: %v\n", err)
+	}
+
+	client := redis.NewClient(&redis.Options{
+		Addr:         mock.Addr(),
+		MinIdleConns: cfg.Redis.MinIdleConn,
+		PoolSize:     cfg.Redis.PoolSize,
+		PoolTimeout:  time.Duration(cfg.Redis.PoolTimeout),
+	})
+
+	if err := client.Ping().Err(); err != nil {
+		t.Errorf("Error while starting redis client: %v\n", err)
+	}
+
+	return client
+}
+
 func TestCreateSession(t *testing.T) {
+	redisConn := ConnRedis(t)
+	redisRepo := NewRedisRepo(log, redisConn, cfg)
+
 	tc := []struct {
 		userId       string
 		userName     string
@@ -30,8 +54,13 @@ func TestCreateSession(t *testing.T) {
 	}
 
 	for _, val := range tc {
-		if err := redisLogic.CreateSession(ctx, val.userId, val.userName, val.userEmail, val.sessionToken); err != nil {
-			t.Errorf("Error while calling CreateSession(): %v\n", err)
+		if err := redisRepo.CreateSession(
+			ctx,
+			val.userId,
+			val.userName,
+			val.userEmail,
+			val.sessionToken); err != nil {
+			t.Errorf("Error while calling CreateSession() method: %v\n", err)
 		}
 	}
 }
