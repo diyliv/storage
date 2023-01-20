e534a14b14e62450e71c39be89db0eb8c1383737
diyliv <onlogn081@gmail.com>
2023-01-20 18:36:59 +0300

    path in ReadConfig() call

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,15 +7,19 @@ import (
 	"github.com/diyliv/storage/internal/storage/usecase"
 	"github.com/diyliv/storage/pkg/logger"
 	"github.com/diyliv/storage/pkg/storage/postgres"
+	"github.com/diyliv/storage/pkg/storage/redis"
 )
 
 func main() {
-	cfg := config.ReadConfig()
+	cfg := config.ReadConfig("./config")
 	logger := logger.InitLogger()
+
+	redisConn := redis.ConnRedis(cfg)
+	redisRepo := repository.NewRedisRepo(logger, redisConn, cfg)
 
 	psqlConn := postgres.ConnPostgres(cfg)
 	psqlRepo := repository.NewPostgresRepository(logger, psqlConn)
-	psqlUC := usecase.NewStorageUC(psqlRepo)
+	psqlUC := usecase.NewStorageUC(psqlRepo, redisRepo)
 
 	server := server.NewServer(logger, cfg, psqlUC)
 	server.StartgRPC()
