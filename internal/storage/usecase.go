1bfd539ad2ccef81783c7a5e34fc8d90f854bf0d
diyliv <onlogn081@gmail.com>
2023-01-21 19:52:36 +0300

    usecase delete method

--- internal/storage/usecase.go
+++ internal/storage/usecase.go
@@ -10,6 +10,7 @@ type Usecase interface {
 	Register(ctx context.Context, user models.User) error
 	GetUserInfo(ctx context.Context, email string) (models.User, error)
 	SavePublicKey(ctx context.Context, userId int, key, passPhrase string) error
+	DeleteUserByEmail(ctx context.Context, email string) error
 	CreateSession(ctx context.Context, userId, userName, userEmail, sessionToken string) error
 	GetSessionInfo(ctx context.Context, sessionToken string) (map[string]string, error)
 	CheckToken(ctx context.Context, sessionToken string) error
