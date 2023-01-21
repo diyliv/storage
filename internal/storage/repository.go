7125192ff74b49ef2b26f067009471a88dbda2e9
diyliv <onlogn081@gmail.com>
2023-01-21 14:36:09 +0300

    getsessioninfo in redis interface

--- internal/storage/repository.go
+++ internal/storage/repository.go
@@ -9,10 +9,11 @@ import (
 type PostgresRepository interface {
 	Register(ctx context.Context, user models.User) error
 	GetUserInfo(ctx context.Context, email string) (models.User, error)
-	SavePublicKey(ctx context.Context, key string) error
+	SavePublicKey(ctx context.Context, userId int, key, passPhrase string) error
 }
 
 type RedisRepository interface {
 	CreateSession(ctx context.Context, userId, userName, userEmail, sessionToken string) error
 	CheckToken(ctx context.Context, sessionToken string) error
+	GetSessionInfo(ctx context.Context, sessionToken string) (map[string]string, error)
 }
