94824059e2d86ead41de47adf1a3a4e0a3df6a32
diyliv <onlogn081@gmail.com>
2023-01-24 16:51:02 +0300

    some changes

--- internal/storage/repository/postgres.go
+++ internal/storage/repository/postgres.go
@@ -42,7 +42,7 @@ func (p *postgresRepo) Register(ctx context.Context, user models.User) error {
 func (p *postgresRepo) GetUserInfo(ctx context.Context, email string) (models.User, error) {
 	var user models.User
 
-	rows, err := p.psql.Query("SELECT user_id, user_name, user_email, user_hashed_password FROM users WHERE user_email = $1", email)
+	rows, err := p.psql.Query("SELECT user_id, user_name, user_hashed_password FROM users WHERE user_email = $1", email)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return models.User{}, errs.ErrNotFound
@@ -52,7 +52,7 @@ func (p *postgresRepo) GetUserInfo(ctx context.Context, email string) (models.Us
 	}
 
 	for rows.Next() {
-		if err := rows.Scan(&user.Id, &user.UserName, &user.UserEmail, &user.UserHashedPassword); err != nil {
+		if err := rows.Scan(&user.Id, &user.UserName, &user.UserHashedPassword); err != nil {
 			p.logger.Error("Error while scanning values: " + err.Error())
 			return models.User{}, err
 		}
@@ -80,60 +80,4 @@ func (p *postgresRepo) DeleteUserByEmail(ctx context.Context, email string) erro
 		return err
 	}
 	return nil
-}
-
-func (p *postgresRepo) Up() error {
-	ctx := context.Background()
-
-	query := "CREATE TABLE IF NOT EXISTS users (" +
-		"user_id SERIAL PRIMARY KEY," +
-		"user_name VARCHAR(16) NOT NULL," +
-		"user_email VARCHAR(32) NOT NULL UNIQUE," +
-		"user_hashed_password VARCHAR NOT NULL," +
-		"user_updated_password TIMESTAMP DEFAULT NOW() NOT NULL," +
-		"user_created_at TIMESTAMP DEFAULT NOW() NOT NULL);" +
-
-		"CREATE TABLE IF NOT EXISTS users_keys(" +
-		"user_id INT NOT NULL," +
-		"user_public_key VARCHAR NOT NULL," +
-		"user_passphrase VARCHAR(16) NOT NULL);"
-
-	stmt, err := p.psql.PrepareContext(ctx, query)
-	if err != nil {
-		p.logger.Error("Error while creating tables: " + err.Error())
-		return err
-	}
-
-	stmt.Close()
-
-	_, err = stmt.ExecContext(ctx)
-	if err != nil {
-		p.logger.Error("Error while executing context: " + err.Error())
-		return err
-	}
-	return nil
-}
-
-func (p *postgresRepo) Drop() error {
-	ctx := context.Background()
-
-	query := "DROP TABLE IF EXISTS users;" + "DROP TABLE IF EXISTS users_keys"
-
-	stmt, err := p.psql.PrepareContext(ctx, query)
-	if err != nil {
-		p.logger.Error("Error while droppping tables: " + err.Error())
-		return err
-	}
-	stmt.Close()
-
-	_, err = stmt.ExecContext(ctx)
-	if err != nil {
-		p.logger.Error("Error while executing context: " + err.Error())
-		return err
-	}
-	return nil
-}
-
-func (p *postgresRepo) Close() {
-	p.psql.Close()
 }
